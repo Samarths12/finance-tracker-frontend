f2397bbd06b7adf8100f5b82da462b600387119c
Abbas Zaidi <abbasrazazaidi731@gmail.com>
2024-10-25 13:20:21 +0530

    second commit

---
+++ src/context/globalContext.js
@@ -0,0 +1,128 @@
+import React, { useContext, useState } from "react";
+import axios from "axios";
+
+const BASE_URL = "https://finance-tracker-es0o.onrender.com/api/v1/";
+const GlobalContext = React.createContext();
+
+export const GlobalProvider = ({ children }) => {
+  const [incomes, setIncomes] = useState([]);
+  const [expenses, setExpenses] = useState([]);
+  const [error, setError] = useState(null);
+
+  // Calculate incomes
+  const addIncome = async (income) => {
+    try {
+      const response = await axios.post(`${BASE_URL}add-income`, income);
+      getIncomes();
+    } catch (err) {
+      setError(err.response ? err.response.data.message : err.message);
+      console.error("Add Income Error:", err);
+    }
+  };
+
+  const getIncomes = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}get-incomes`);
+      setIncomes(response.data);
+      console.log(response.data);
+    } catch (err) {
+      setError(err.response ? err.response.data.message : err.message);
+      console.error("Get Incomes Error:", err);
+    }
+  };
+
+  const deleteIncome = async (id) => {
+    try {
+      const res = await axios.delete(`${BASE_URL}delete-income/${id}`);
+      getIncomes();
+    } catch (err) {
+      setError(err.response ? err.response.data.message : err.message);
+      console.error("Delete Income Error:", err);
+    }
+  };
+
+  const totalIncome = () => {
+    let totalIncome = 0;
+    incomes.forEach((income) => {
+      totalIncome += income.amount;
+    });
+    return totalIncome;
+  };
+
+  // Calculate expenses
+  const addExpense = async (expense) => {
+    try {
+      const response = await axios.post(`${BASE_URL}add-expense`, expense);
+      getExpenses();
+    } catch (err) {
+      setError(err.response ? err.response.data.message : err.message);
+      console.error("Add Expense Error:", err);
+    }
+  };
+
+  const getExpenses = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}get-expenses`);
+      setExpenses(response.data);
+      console.log(response.data);
+    } catch (err) {
+      setError(err.response ? err.response.data.message : err.message);
+      console.error("Get Expenses Error:", err);
+    }
+  };
+
+  const deleteExpense = async (id) => {
+    try {
+      const res = await axios.delete(`${BASE_URL}delete-expense/${id}`);
+      getExpenses();
+    } catch (err) {
+      setError(err.response ? err.response.data.message : err.message);
+      console.error("Delete Expense Error:", err);
+    }
+  };
+
+  const totalExpenses = () => {
+    let totalExpenses = 0;
+    expenses.forEach((expense) => {
+      totalExpenses += expense.amount;
+    });
+    return totalExpenses;
+  };
+
+  const totalBalance = () => {
+    return totalIncome() - totalExpenses();
+  };
+
+  const transactionHistory = () => {
+    const history = [...incomes, ...expenses];
+    history.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+    return history.slice(0, 3);
+  };
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        addIncome,
+        getIncomes,
+        incomes,
+        deleteIncome,
+        expenses,
+        totalIncome,
+        addExpense,
+        getExpenses,
+        deleteExpense,
+        totalExpenses,
+        totalBalance,
+        transactionHistory,
+        error,
+        setError,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(GlobalContext);
+};
